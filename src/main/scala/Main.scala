ba23c1ed23a8f41eedb0e9677b7ac21640312d23
P, Jaimi <jaimi.patel19@imperial.ac.uk>
2021-02-06 16:48:10 +0000

    maintenance: Remove dummy test
    Co-authored-by: R, Alex <alex.richardson19@imperial.ac.uk>
    
    Co-authored-by: S, Devam <devam.savjani19@imperial.ac.uk>
    
    Co-authored-by: D, Tyrell <tyrell.duku19@imperial.ac.uk>

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,5 @@
 object Main {
   def main(args: Array[String]) = {
     println("Hello, World")
-    Parser.run()
-  }
-
-  def succ(x: Int): Int = {
-    x + 1
   }
 }
--- src/test/scala/MainTest.scala
+++
@@ -1,8 +0,0 @@
-import org.scalatest.funsuite.AnyFunSuite
-
-class MainTest extends AnyFunSuite {
-
-  test("succ increments by 1 correctly") {
-    assert(Main.succ(10) == 11)
-  }
-}
