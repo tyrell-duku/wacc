7944800a6b83ba1020468e2d8f8ddb434418b73d
P, Jaimi <jaimi.patel19@imperial.ac.uk>
2021-02-11 22:09:21 +0000

    feat(SemanticError): Add invalid return
    - Invalid return from Main
    Co-authored-by: R, Alex <alex.richardson19@imperial.ac.uk>

--- src/main/scala/SemanticError.scala
+++ src/main/scala/SemanticError.scala
@@ -3,13 +3,14 @@ import Rules._
 sealed trait SemanticError {
   override def toString: String = this match {
     case typeMismatch(invalid, actualT, expected) =>
-      "Type mismatch: LHS of type " + expected + "but RHS of type " + actualT
+      "Type mismatch at " + invalid + "LHS is of type " + expected + "but RHS of type " + actualT
     case variableNotDeclared(id) =>
       "Variable" + id + "not declared in current scope"
     case variableDeclared(id) => "Conflicting definitions for variable " + id
     case arrayOutOfBounds(id) => "Array index out of bounds for array " + id
     case elementAccessDenied(id) =>
       "Element access of " + id + " is not permitted"
+    case invalidReturn(e) => "Invalid return statement from main: return " + e
   }
 }
 
@@ -19,3 +20,4 @@ case class variableNotDeclared(id: Ident) extends SemanticError
 case class variableDeclared(id: Ident) extends SemanticError
 case class arrayOutOfBounds(id: Ident) extends SemanticError
 case class elementAccessDenied(id: Ident) extends SemanticError
+case class invalidReturn(e: Expr) extends SemanticError
