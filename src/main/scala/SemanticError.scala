ec0c698a99d3e1f0826475af1027c749e58c63b8
Devam <devamsavjani@rocketmail.com>
2021-02-11 14:04:05 +0000

    feat: SemanticError trait for semantic errors
    Co-authored-by: D, Tyrell <tyrell.duku19@imperial.ac.uk>
    
    Co-authored-by: P, Jaimi <jaimi.patel19@imperial.ac.uk>
    
    Co-authored-by: R, Alex <alex.richardson19@imperial.ac.uk>

---
+++ src/main/scala/SemanticError.scala
@@ -0,0 +1,18 @@
+import Rules._
+
+sealed trait SemanticError {
+  override def toString(): String = this match {
+    case typeMismatch(invalid, actualT, expected) =>
+      "Type mismatch: LHS of type " + expected + "but RHS of type " + actualT
+    case variableNotDeclared(id) =>
+      "Variable" + id + "not declared in current scope"
+    case variableDeclared(id) => "Conflicting definitions for variable " + id
+    case arrayOutOfBounds(id) => "Array index out of bounds for array " + id
+  }
+}
+
+case class typeMismatch(invalid: AssignRHS, actualT: Type, expected: Type)
+    extends SemanticError
+case class variableNotDeclared(id: Ident) extends SemanticError
+case class variableDeclared(id: Ident) extends SemanticError
+case class arrayOutOfBounds(id: Ident) extends SemanticError
