08e66eab1783b4942cf698d6c67b84a8385fe7e8
P, Jaimi <jaimi.patel19@imperial.ac.uk>
2021-02-12 01:40:54 +0000

    feat: Add invalid error type messages
    Co-authored-by: R, Alex <alex.richardson19@imperial.ac.uk>
    
    Co-authored-by: S, Devam <devam.savjani19@imperial.ac.uk>
    
    Co-authored-by: D, Tyrell <tyrell.duku19@imperial.ac.uk>

--- src/main/scala/SemanticError.scala
+++ src/main/scala/SemanticError.scala
@@ -3,17 +3,23 @@ import Rules._
 sealed trait SemanticError {
   override def toString: String = this match {
     case typeMismatch(invalid, actualT, expected) =>
-      "Type mismatch at " + invalid + "LHS is of type " + expected.mkString(
+      "Type mismatch at " + invalid + " expected type " + expected.mkString(
         " or "
-      ) + "but RHS of type " + actualT
+      ) + " but actual type " + actualT
     case variableNotDeclared(id) =>
-      "Variable" + id + "not declared in current scope"
+      "Variable " + id + " not declared in current scope"
     case variableDeclared(id) => "Conflicting definitions for variable " + id
     case arrayOutOfBounds(id) => "Array index out of bounds for array " + id
     case elementAccessDenied(id) =>
       "Element access of " + id + " is not permitted"
-    case invalidReturn(e)              => "Invalid return statement from main: return " + e
-    case functionIllegalAssignment(id) => "Illegal assignment to function " + id
+    case invalidReturn(e) => "Invalid return statement from main: return " + e
+    case functionIllegalAssignment(id) =>
+      "Illegal assignment to function " + id.s
+    case functionNotDeclared(id) => "Function " + id.s + " not declared"
+    case invalidParams(id, actual, expected) =>
+      "Invalid params for function " + id.s + " Expected number of params: " + expected + ". Actual: " + actual
+    case invalidPairElem(pe) =>
+      "Invalid input, expected: pair, unable to perform " + pe
   }
 }
 
@@ -25,3 +31,7 @@ case class arrayOutOfBounds(id: Ident) extends SemanticError
 case class elementAccessDenied(id: Ident) extends SemanticError
 case class invalidReturn(e: Expr) extends SemanticError
 case class functionIllegalAssignment(id: Ident) extends SemanticError
+case class functionNotDeclared(id: Ident) extends SemanticError
+case class invalidParams(id: Ident, actual: Int, expected: Int)
+    extends SemanticError
+case class invalidPairElem(e: Expr) extends SemanticError
