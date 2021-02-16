ca92b303d923f0a124f6077d7cd692a84b0b8ce2
R, Alex <alex.richardson19@imperial.ac.uk>
2021-02-16 21:33:47 +0000

    feat(Main): Add syntaxError & semanticError
    - Remove magic numbers for exit numbers

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,16 @@
 import java.io.File
 
 object Main {
+  val syntaxError = 100
+  val semanticError = 200
+
   def main(args: Array[String]) = {
     if (args.length == 0) {
       throw new Exception("Invalid number of arguments, please enter a file.")
     }
 
     val file = new File(args(0))
-    if (!file.exists()){
+    if (!file.exists()) {
       throw new Exception("File not present, please enter a valid file.")
     }
 
@@ -16,16 +19,16 @@ object Main {
     if (parserResult.isFailure) {
       println("Syntax Error")
       println(parserResult)
-      sys.exit(100)
+      sys.exit(syntaxError)
     }
 
     val semanticChecker = new SemanticChecker
-    val semanticResult  = semanticChecker.progAnalysis(parserResult.get)
+    val semanticResult = semanticChecker.progAnalysis(parserResult.get)
 
-    if (semanticResult.nonEmpty){
+    if (semanticResult.nonEmpty) {
       println("Semantic Error")
       semanticResult.foreach((semErr: SemanticError) => println(semErr))
-      sys.exit(200)
+      sys.exit(semanticError)
     }
   }
 }
