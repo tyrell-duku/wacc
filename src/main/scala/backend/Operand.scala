45985fbe2ce019c0dbbf5edfebfb5c8f4bf076bf
P, Jaimi <jaimi.patel19@imperial.ac.uk>
2021-02-20 15:43:51 +0000

    feat(Operand): Add toString methods

--- src/main/scala/backend/Operand.scala
+++ src/main/scala/backend/Operand.scala
@@ -2,23 +2,62 @@ package backend
 
 sealed trait Operand
 
-sealed class Imm(n: Int) extends Operand
+sealed class Imm(n: Int) extends Operand {
+  override def toString: String = "#" + n
+}
 
 sealed trait Reg extends Operand
-case object R0 extends Reg
-case object R1 extends Reg
-case object R2 extends Reg
-case object R3 extends Reg
-case object R4 extends Reg
-case object R5 extends Reg
-case object R6 extends Reg
-case object R7 extends Reg
-case object R8 extends Reg
-case object R9 extends Reg
-case object R10 extends Reg
-case object R11 extends Reg
-case object R12 extends Reg
-case object R13 extends Reg
-case object R14 extends Reg
+case object R0 extends Reg {
+  override def toString: String = "r0"
+}
+case object R1 extends Reg {
+  override def toString: String = "r1"
+}
+case object R2 extends Reg {
+  override def toString: String = "r2"
+}
+case object R3 extends Reg {
+  override def toString: String = "r3"
+}
+case object R4 extends Reg {
+  override def toString: String = "r4"
+}
+case object R5 extends Reg {
+  override def toString: String = "r5"
+}
+case object R6 extends Reg {
+  override def toString: String = "r6"
+}
+case object R7 extends Reg {
+  override def toString: String = "r7"
+}
+case object R8 extends Reg {
+  override def toString: String = "r8"
+}
+case object R9 extends Reg {
+  override def toString: String = "r9"
+}
+case object R10 extends Reg {
+  override def toString: String = "r10"
+}
+case object R11 extends Reg {
+  override def toString: String = "r11"
+}
+case object R12 extends Reg {
+  override def toString: String = "r12"
+}
 
-// TODO: Stack Pointer, Instruction Register, Program Counter
+// Stack Pointer
+case object SP extends Reg {
+  override def toString: String = "sp"
+}
+
+// Link Register
+case object LR extends Reg {
+  override def toString: String = "lr"
+}
+
+// Program Counter
+case object PC extends Reg {
+  override def toString: String = "pc"
+}
