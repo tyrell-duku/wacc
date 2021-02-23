f7e313755c0c8989ad1c7eaee15f49007c6bdbc1
Devam <devamsavjani@rocketmail.com>
2021-02-23 13:45:28 +0000

    feat(DataTable): Ret msg label from addDataEntry
    Co-authored-by: P, Jaimi <jaimi.patel19@imperial.ac.uk>

--- src/main/scala/backend/DataTable.scala
+++ src/main/scala/backend/DataTable.scala
@@ -3,27 +3,23 @@ package backend
 import scala.collection.mutable.ListBuffer
 import InstructionSet.Data
 import InstructionSet.Label
+import frontend.Rules.Character
 
 class DataTable {
   var table = ListBuffer.empty[Data]
   val stringDataSkeleton = "msg_"
   var dataCount = 0
 
-  def addDataEntry(string: List[Character]) = {
-    table += (Data(Label(getNextLabel()), string.toString()))
+  def addDataEntry(string: List[Character]): Label = {
+    val msgLabel = Label(getNextLabel())
+    table += Data(msgLabel, string.toString())
+    msgLabel
   }
 
   private def getNextLabel(): String = {
+    val nextLabel = stringDataSkeleton + dataCount.toString()
     dataCount += 1
-    stringDataSkeleton + dataCount.toString()
-  }
-
-  def getCurrLabel(): String = {
-    stringDataSkeleton + dataCount.toString()
-  }
-
-  override def toString() = {
-    table.toString()
+    nextLabel
   }
 
 }
