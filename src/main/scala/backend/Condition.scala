42c14d3b675a65dd3cc46b507fc64e61fc1690b6
R, Alex <alex.richardson19@imperial.ac.uk>
2021-02-24 15:37:21 +0000

    feat(Condition): Add GTE & LTE

--- src/main/scala/backend/Condition.scala
+++ src/main/scala/backend/Condition.scala
@@ -4,4 +4,6 @@ trait Condition
 case object EQ extends Condition
 case object NE extends Condition
 case object LT extends Condition
+case object LTE extends Condition
 case object GT extends Condition
+case object GTE extends Condition
