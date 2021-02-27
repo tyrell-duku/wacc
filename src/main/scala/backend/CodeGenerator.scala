d9734a6868bc2e02aab6ff61671dc2923b4b27f9
P, Jaimi <jaimi.patel19@imperial.ac.uk>
2021-02-27 17:34:00 +0000

    feat(CodeGenerator): Add Negation UnOp
    Co-authored-by: R, Alex <alex.richardson19@imperial.ac.uk>
    
    Co-authored-by: S, Devam <devam.savjani19@imperial.ac.uk>
    
    Co-authored-by: D, Tyrell <tyrell.duku19@imperial.ac.uk>

--- src/main/scala/backend/CodeGenerator.scala
+++ src/main/scala/backend/CodeGenerator.scala
@@ -239,9 +239,11 @@ object CodeGenerator {
   /* Translates unary operator OP to the internal representation. */
   private def transUnOp(op: UnOp, reg: Reg): Unit = {
     op match {
-      case Chr(e, _)        => transExp(e, reg)
-      case Len(e, pos)      => ListBuffer.empty
-      case Negation(e, pos) => ListBuffer.empty
+      case Chr(e, _)   => transExp(e, reg)
+      case Len(e, pos) => ListBuffer.empty
+      case Negation(e, _) =>
+        transExp(e, reg)
+        instructions += NegInstr(reg, reg)
       case Not(e, _) =>
         transExp(e, reg)
         instructions += Eor(reg, reg, ImmInt(1))
