ed31f55fa12bb692f423545beb5cafaf917c80c1
R, Alex <alex.richardson19@imperial.ac.uk>
2021-02-27 23:07:48 +0000

    feat(hook-scripts): Add pre-push script

--- hook-scripts/install-hooks.bash
+++ hook-scripts/install-hooks.bash
@@ -4,9 +4,10 @@
 
 GIT_DIR=$(git rev-parse --git-dir)
 
-chmod +x $GIT_DIR/../hook-scripts/commit-msg.py
+chmod +x commit-msg.py pre-push.bash
 
 echo "Installing hooks..."
 # symlinks to the pre-commit script
 ln -sf $GIT_DIR/../hook-scripts/commit-msg.py $GIT_DIR/hooks/commit-msg
+ln -sf $GIT_DIR/../hook-scripts/pre-push.bash $GIT_DIR/hooks/pre-push
 echo "Done!"
