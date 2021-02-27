e920f538f4fe8885702f996b375aa4aaf5ed451c
R, Alex <alex.richardson19@imperial.ac.uk>
2021-02-27 22:55:37 +0000

    feat(install-scripts): Add chmod +x scripts

--- hook-scripts/install-hooks.bash
+++ hook-scripts/install-hooks.bash
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-# Run the command `chmod +x .` in the scripts directory
+# Run the command `chmod +x ./install-hooks.bash` in the scripts directory
 
 GIT_DIR=$(git rev-parse --git-dir)
+
+chmod +x $GIT_DIR/../hook-scripts/commit-msg.py
 
 echo "Installing hooks..."
 # symlinks to the pre-commit script
