98ece9d922d6ee68c57512230046f02ac3ce5b3c
R, Alex <alex.richardson19@imperial.ac.uk>
2021-03-02 19:00:30 +0000

    feat(ReadInstructions): Add Read Labels
    - Add p_read_char & p_read_int
    - For transReadIdent
    
    Co-authored-by: D, Tyrell <tyrell.duku19@imperial.ac.uk>
    
    Co-authored-by: P, Jaimi <jaimi.patel19@imperial.ac.uk>

---
+++ src/main/scala/backend/ReadInstructions.scala
@@ -0,0 +1,36 @@
+package backend
+
+import InstructionSet._
+import frontend.Rules._
+import scala.collection.mutable.ListBuffer
+
+object ReadInstructions {
+
+  final val resultReg: Reg = R0
+
+  def charRead(label: Label): (Label, List[Instruction]) = {
+    (
+      Label("p_read_char"),
+      List[Instruction](
+        Push(ListBuffer(LR)),
+        Ldr(resultReg, DataLabel(label)),
+        Add(resultReg, resultReg, ImmInt(4)),
+        BranchLink(Label("scanf")),
+        Pop(ListBuffer(PC))
+      )
+    )
+  }
+
+  def intRead(label: Label): (Label, List[Instruction]) = {
+    (
+      Label("p_read_int"),
+      List[Instruction](
+        Mov(R1, resultReg),
+        Ldr(resultReg, DataLabel(label)),
+        Add(resultReg, resultReg, ImmInt(4)),
+        BranchLink(Label("scanf")),
+        Pop(ListBuffer(PC))
+      )
+    )
+  }
+}
