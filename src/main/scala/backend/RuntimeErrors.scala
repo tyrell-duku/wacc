3d7835ec2adc97384e9f54e1bcd62b0d1ac20e1e
R, Alex <alex.richardson19@imperial.ac.uk>
2021-03-02 22:33:58 +0000

    feat(RuntimeErrors): Add checkArraryBounds
    - Use instructions from 5b4e03b

--- src/main/scala/backend/RuntimeErrors.scala
+++ src/main/scala/backend/RuntimeErrors.scala
@@ -1,7 +1,5 @@
 package backend
 
-package backend
-
 import InstructionSet._
 import frontend.Rules._
 import scala.collection.mutable.ListBuffer
@@ -15,6 +13,26 @@ object RuntimeErrors {
         BranchLink(Label("p_print_string")),
         Mov(R0, ImmInt(-1)),
         BranchLink(Label("exit"))
+      )
+    )
+  }
+
+  def checkArrayBounds(
+      label1: Label,
+      label2: Label
+  ): (Label, List[Instruction]) = {
+    (
+      Label("p_check_array_bounds"),
+      List[Instruction](
+        Push(ListBuffer(LR)),
+        Cmp(R0, ImmInt(0)),
+        LdrLT(R0, DataLabel(label1)),
+        BranchLinkLT(Label("p_throw_runtime_error")),
+        Ldr(R1, RegAdd(R1)),
+        Cmp(R0, R1),
+        LdrCS(R0, label2),
+        BranchLinkCS(Label("p_throw_runtime_error")),
+        Pop(ListBuffer(PC))
       )
     )
   }
