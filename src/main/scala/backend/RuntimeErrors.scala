ddab70a90bc3d1ddd19bf467a337e67dec048197
R, Alex <alex.richardson19@imperial.ac.uk>
2021-03-02 19:48:23 +0000

    feat(RuntimeErrors): Add throwRuntimeError
    - Pre-determined instructions for throwing a runtime error
    Co-authored-by: D, Tyrell <tyrell.duku19@imperial.ac.uk>

--- src/main/scala/backend/RuntimeErrors.scala
+++ src/main/scala/backend/RuntimeErrors.scala
@@ -6,4 +6,16 @@ import InstructionSet._
 import frontend.Rules._
 import scala.collection.mutable.ListBuffer
 
-object RuntimeErrors {}
+object RuntimeErrors {
+
+  def throwRuntimeError(): (Label, List[Instruction]) = {
+    (
+      Label("p_throw_runtime_error"),
+      List[Instruction](
+        BranchLink(Label("p_print_string")),
+        Mov(R0, ImmInt(-1)),
+        BranchLink(Label("exit"))
+      )
+    )
+  }
+}
