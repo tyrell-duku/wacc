a649005577767723cc0c6ce47583d8e5a3964fc5
S, Devam <devam.savjani19@imperial.ac.uk>
2021-03-04 21:45:50 +0000

    test(BackendRuntimeErrTest): Remove .out files

--- src/test/scala/backend/BackendRuntimeErrTest.scala
+++ src/test/scala/backend/BackendRuntimeErrTest.scala
@@ -4,10 +4,11 @@ import java.io.File
 
 class BackendRuntimeErrTest extends AnyFunSuite {
   for (f <- getFilesFrom("wacc_examples/valid/runtimeErr/")) {
-    val name = f.getName
+    val name = f.getName()
     val (file, out, command) = createOutputFiles(f)
     test(s"Runtime error exit code test for $name") {
       assert(checkExitCode(file, out, command))
+      out.delete()
     }
   }
 }
