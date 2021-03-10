c97e6a984bf24e98c40e81fe30ceba1b97263552
R, Alex <alex.richardson19@imperial.ac.uk>
2021-03-10 19:13:15 +0000

    feat(HeapParser): Add syntax error codes
    Co-authored-by: P, Jaimi <jaimi.patel19@imperial.ac.uk>

--- src/main/scala/frontend/Parser/HeapParser.scala
+++ src/main/scala/frontend/Parser/HeapParser.scala
@@ -19,17 +19,21 @@ object HeapParser {
   // '*' '('<expr>')'
   val derefPtr: Parsley[DerefPtr] = DerefPtr(
     "*" *> (lexer.parens(expr) <|> identifier)
-  )
+  ) ? "derefrenced pointer"
 
   // "malloc" '('<expr>')' | "realloc" '('<ident>, <expr>')' | "calloc" '('<expr>, <type>')'
   val memoryAlloc: Parsley[MemoryAlloc] =
-    Malloc("malloc" *> lexer.parens(expr)) <|> ("realloc" *> lexer.parens(
+    (Malloc("malloc" *> lexer.parens(expr)) <|> ("realloc" *> lexer.parens(
       Realloc(identifier, "," *> expr)
-    )) <|> ("calloc" *> lexer.parens(Calloc(expr, "," *> expr)))
+    )) <|> ("calloc" *> lexer.parens(
+      Calloc(expr, "," *> expr)
+    ))) ? "memory allocation function"
 
   // '&'<expr>
-  val addr: Parsley[Addr] = Addr("&" *> (lexer.parens(expr) <|> identifier))
+  val addr: Parsley[Addr] =
+    Addr("&" *> (lexer.parens(expr) <|> identifier)) ? "address operator"
 
   // "sizeof" '('<type>')'
-  val sizeOf: Parsley[SizeOf] = SizeOf("sizeof" *> lexer.parens(types))
+  val sizeOf: Parsley[SizeOf] =
+    SizeOf("sizeof" *> lexer.parens(types)) ? "sizeof function"
 }
