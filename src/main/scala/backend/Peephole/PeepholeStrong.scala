fb3a015fe45415633ecd66fa35f5d6ba25601aa5
S, Devam <devam.savjani19@imperial.ac.uk>
2021-03-11 13:41:17 +0000

    refactor(PeepholeStrong): Mul pattern matching

--- src/main/scala/backend/Peephole/PeepholeStrong.scala
+++ src/main/scala/backend/Peephole/PeepholeStrong.scala
@@ -12,11 +12,16 @@ object PeepholeStrong {
 
   val LOG_ERROR = -1
 
-  def log2(i: Int): Double = {
+  def getShiftAmount(i: Int): Int = {
     if (i == 0) {
       LOG_ERROR
     } else {
-      log(i) / log(2.0)
+      val dVal = log(i) / log(2.0)
+      if (dVal == floor(dVal)) {
+        dVal.toInt
+      } else {
+        LOG_ERROR
+      }
     }
   }
 
@@ -28,45 +33,28 @@ object PeepholeStrong {
       instructionsBuff: mutable.ListBuffer[Instruction],
       remainingTail: mutable.ListBuffer[Instruction]
   ): mutable.ListBuffer[Instruction] = {
-    val operation = remainingTail.head
-    if (operation == SMul(r1, r2, r1, r2)) {
-      op2 match {
-        case ImmMem(n) =>
-          val shiftAmount = log2(n)
-          if (shiftAmount == floor(shiftAmount) && shiftAmount != LOG_ERROR) {
-            instructionsBuff += Ldr(r1, op1)
-
-            if (shiftAmount != 0) {
-              instructionsBuff += Mov(r1, LSL(r1, ImmInt(shiftAmount.toInt)))
-              instructionsBuff += BranchLinkCond(
-                VS,
-                Overflow.funcLabel
-              )
+    if (remainingTail.head == SMul(r1, r2, r1, r2)) {
+      (op1, op2) match {
+        case (ImmMem(n1), ImmMem(n2)) =>
+          val shiftAmount1 = getShiftAmount(n1)
+          val shiftAmount2 = getShiftAmount(n2)
+          if (shiftAmount1 > shiftAmount2 && shiftAmount1 != LOG_ERROR) {
+            instructionsBuff += Ldr(r1, op2)
+            if (shiftAmount1 != 0) {
+              instructionsBuff += Mov(r1, LSL(r1, ImmInt(shiftAmount1)))
             }
-
             instructionsBuff ++= optimise(
               remainingTail.tail.tail.tail.head,
               remainingTail.tail.tail.tail.tail
             )
             return instructionsBuff
-          }
-        case _ =>
-      }
-
-      op1 match {
-        case ImmMem(n) =>
-          val shiftAmount = log2(n)
-          if (shiftAmount == floor(shiftAmount) && shiftAmount != LOG_ERROR) {
-            instructionsBuff += Ldr(r1, op2)
-
-            if (shiftAmount != 0) {
-              instructionsBuff += Mov(r1, LSL(r1, ImmInt(shiftAmount.toInt)))
-              instructionsBuff += BranchLinkCond(
-                VS,
-                Overflow.funcLabel
-              )
+          } else if (
+            shiftAmount1 <= shiftAmount2 && shiftAmount2 != LOG_ERROR
+          ) {
+            instructionsBuff += Ldr(r1, op1)
+            if (shiftAmount2 != 0) {
+              instructionsBuff += Mov(r1, LSL(r1, ImmInt(shiftAmount2)))
             }
-
             instructionsBuff ++= optimise(
               remainingTail.tail.tail.tail.head,
               remainingTail.tail.tail.tail.tail
@@ -102,17 +90,14 @@ object PeepholeStrong {
               )
               return instructionsBuff
             }
-            val shiftAmount = log2(n)
-            if (
-              shiftAmount == floor(shiftAmount) &&
-              !(shiftAmount == 0 || shiftAmount == LOG_ERROR)
-            ) {
+            val shiftAmount = getShiftAmount(n)
+            if (!(shiftAmount == 0 || shiftAmount == LOG_ERROR)) {
               remainingTail.head match {
                 case Mov(rd, _) =>
                   instructionsBuff += Ldr(r1, op1)
                   instructionsBuff += Mov(
                     r1,
-                    LSR(r1, ImmInt(shiftAmount.toInt))
+                    LSR(r1, ImmInt(shiftAmount))
                   )
                   instructionsBuff += Mov(rd, r1)
                   instructionsBuff ++= optimise(
