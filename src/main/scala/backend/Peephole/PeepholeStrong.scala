0ed9eeb7f42eb38ef8423020457c1f6de9d01b02
S, Devam <devam.savjani19@imperial.ac.uk>
2021-03-11 14:54:28 +0000

    refactor(PeepholeStrong): Add divisionReduction

--- src/main/scala/backend/Peephole/PeepholeStrong.scala
+++ src/main/scala/backend/Peephole/PeepholeStrong.scala
@@ -25,6 +25,56 @@ object PeepholeStrong {
     }
   }
 
+  def divisionReduction(
+      r1: Reg,
+      op1: LoadOperand,
+      r2: Reg,
+      op2: LoadOperand,
+      instructionsBuff: mutable.ListBuffer[Instruction],
+      remainingTail: mutable.ListBuffer[Instruction]
+  ): mutable.ListBuffer[Instruction] = {
+    op2 match {
+      case ImmMem(0) =>
+        remainingTail.tail.head match {
+          case Mov(rd, r3) =>
+            if (r3 == r2) {
+              instructionsBuff += Ldr(
+                R0,
+                DataLabel(Label("msg_divide_by_zero"))
+              )
+              instructionsBuff += BranchLink(RuntimeError.funcLabel)
+              return instructionsBuff
+            }
+          case _ =>
+        }
+      case ImmMem(n) =>
+        val shiftAmount = getShiftAmount(n)
+        if (!(shiftAmount == LOG_ERROR)) {
+          remainingTail.head match {
+            case Mov(rd, _) =>
+              instructionsBuff += Ldr(r1, op1)
+              if (shiftAmount != 0) {
+                instructionsBuff += Mov(
+                  r1,
+                  LSR(r1, ImmInt(shiftAmount))
+                )
+              }
+              instructionsBuff += Mov(rd, r1)
+              instructionsBuff ++= optimise(
+                remainingTail.tail.tail.tail.tail.head,
+                remainingTail.tail.tail.tail.tail.tail
+              )
+              return instructionsBuff
+            case _ =>
+          }
+        }
+      case _ =>
+    }
+    instructionsBuff += Ldr(r1, op1)
+    instructionsBuff ++= optimise(Ldr(r2, op2), remainingTail)
+    instructionsBuff
+  }
+
   def multiplyReduction(
       r1: Reg,
       op1: LoadOperand,
@@ -33,32 +83,36 @@ object PeepholeStrong {
       instructionsBuff: mutable.ListBuffer[Instruction],
       remainingTail: mutable.ListBuffer[Instruction]
   ): mutable.ListBuffer[Instruction] = {
-    (op1, op2) match {
-      case (ImmMem(n1), ImmMem(n2)) =>
-        val shiftAmount1 = getShiftAmount(n1)
-        val shiftAmount2 = getShiftAmount(n2)
-        if (shiftAmount1 > shiftAmount2 && shiftAmount1 != LOG_ERROR) {
-          instructionsBuff += Ldr(r1, op2)
-          if (shiftAmount1 != 0) {
-            instructionsBuff += Mov(r1, LSL(r1, ImmInt(shiftAmount1)))
+    if (remainingTail.size >= 4) {
+      (op1, op2) match {
+        case (ImmMem(n1), ImmMem(n2)) =>
+          val shiftAmount1 = getShiftAmount(n1)
+          val shiftAmount2 = getShiftAmount(n2)
+          if (shiftAmount1 > shiftAmount2 && shiftAmount1 != LOG_ERROR) {
+            instructionsBuff += Ldr(r1, op2)
+            if (shiftAmount1 != 0) {
+              instructionsBuff += Mov(r1, LSL(r1, ImmInt(shiftAmount1)))
+            }
+            instructionsBuff ++= optimise(
+              remainingTail.tail.tail.tail.head,
+              remainingTail.tail.tail.tail.tail
+            )
+            return instructionsBuff
+          } else if (
+            shiftAmount1 <= shiftAmount2 && shiftAmount2 != LOG_ERROR
+          ) {
+            instructionsBuff += Ldr(r1, op1)
+            if (shiftAmount2 != 0) {
+              instructionsBuff += Mov(r1, LSL(r1, ImmInt(shiftAmount2)))
+            }
+            instructionsBuff ++= optimise(
+              remainingTail.tail.tail.tail.head,
+              remainingTail.tail.tail.tail.tail
+            )
+            return instructionsBuff
           }
-          instructionsBuff ++= optimise(
-            remainingTail.tail.tail.tail.head,
-            remainingTail.tail.tail.tail.tail
-          )
-          return instructionsBuff
-        } else if (shiftAmount1 <= shiftAmount2 && shiftAmount2 != LOG_ERROR) {
-          instructionsBuff += Ldr(r1, op1)
-          if (shiftAmount2 != 0) {
-            instructionsBuff += Mov(r1, LSL(r1, ImmInt(shiftAmount2)))
-          }
-          instructionsBuff ++= optimise(
-            remainingTail.tail.tail.tail.head,
-            remainingTail.tail.tail.tail.tail
-          )
-          return instructionsBuff
-        }
-      case _ =>
+        case _ =>
+      }
     }
     instructionsBuff += Ldr(r1, op1)
     instructionsBuff ++= optimise(Ldr(r2, op2), remainingTail)
@@ -82,53 +136,18 @@ object PeepholeStrong {
         instructionsBuff,
         remainingTail
       )
-    } else if (remainingTail.size > 4)
+    } else if (remainingTail.size >= 4)
       if (
         remainingTail.tail.tail.tail.head == BranchLink(Label("__aeabi_idiv"))
       ) {
-        op2 match {
-          case ImmMem(n) =>
-            if (n == 0) {
-              remainingTail.tail.head match {
-                case Mov(rd, r3) =>
-                  if (r3 == r2) {
-                    instructionsBuff += Ldr(
-                      R0,
-                      DataLabel(Label("msg_divide_by_zero"))
-                    )
-                    instructionsBuff += BranchLink(RuntimeError.funcLabel)
-                    return instructionsBuff
-                  }
-                case _ =>
-              }
-            } else if (n == 1) {
-              instructionsBuff += Ldr(r1, op1)
-              instructionsBuff ++= optimise(
-                Mov(R0, r1),
-                remainingTail.tail.tail.tail.tail
-              )
-              return instructionsBuff
-            }
-            val shiftAmount = getShiftAmount(n)
-            if (!(shiftAmount == 0 || shiftAmount == LOG_ERROR)) {
-              remainingTail.head match {
-                case Mov(rd, _) =>
-                  instructionsBuff += Ldr(r1, op1)
-                  instructionsBuff += Mov(
-                    r1,
-                    LSR(r1, ImmInt(shiftAmount))
-                  )
-                  instructionsBuff += Mov(rd, r1)
-                  instructionsBuff ++= optimise(
-                    remainingTail.tail.tail.tail.tail.head,
-                    remainingTail.tail.tail.tail.tail.tail
-                  )
-                  return instructionsBuff
-                case _ =>
-              }
-            }
-          case _ =>
-        }
+        return divisionReduction(
+          r1,
+          op1,
+          r2,
+          op2,
+          instructionsBuff,
+          remainingTail
+        );
       }
     instructionsBuff += Ldr(r1, op1)
     instructionsBuff ++= optimise(Ldr(r2, op2), remainingTail)
