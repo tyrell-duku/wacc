7bb17da10b671272c8aa89f66939e272ab82f535
R, Alex <alex.richardson19@imperial.ac.uk>
2021-03-11 15:42:11 +0000

    test(ParserIntRepTest): Add overflow tests
    - Tests are currently SKIPPED

--- src/test/scala/frontend/ParserIntRepTest.scala
+++ src/test/scala/frontend/ParserIntRepTest.scala
@@ -4,43 +4,56 @@ import org.scalatest.funsuite.AnyFunSuite
 import frontend.Rules._
 import frontend.LiterParser._
 
-
 class FrontendParserIntRepTest extends AnyFunSuite {
 
   test("Successfully parses octal integer") {
-    assert(intLiter.runParser("0o007").contains(IntLiter(7, (1,1))))
+    assert(intLiter.runParser("0o007").contains(IntLiter(7, (1, 1))))
   }
 
   test("Successfully parses octal integer with positive sign") {
-    assert(intLiter.runParser("+0o007").contains(IntLiter(7, (1,1))))
+    assert(intLiter.runParser("+0o007").contains(IntLiter(7, (1, 1))))
   }
 
   test("Successfully parses octal integer with negative sign") {
-    assert(intLiter.runParser("-0o007").contains(IntLiter(-7, (1,1))))
+    assert(intLiter.runParser("-0o007").contains(IntLiter(-7, (1, 1))))
   }
 
   test("Successfully parses hexadecimal integer") {
-    assert(intLiter.runParser("0x007").contains(IntLiter(7, (1,1))))
+    assert(intLiter.runParser("0x007").contains(IntLiter(7, (1, 1))))
   }
 
   test("Successfully parses hexadecimal integer with positive sign") {
-    assert(intLiter.runParser("+0x007").contains(IntLiter(7, (1,1))))
+    assert(intLiter.runParser("+0x007").contains(IntLiter(7, (1, 1))))
   }
 
   test("Successfully parses hexadecimal integer with negative sign") {
-    assert(intLiter.runParser("-0x006").contains(IntLiter(-6, (1,1))))
+    assert(intLiter.runParser("-0x006").contains(IntLiter(-6, (1, 1))))
   }
 
   ignore("Successfully parses binary integer") {
-    assert(intLiter.runParser("0b001").contains(IntLiter(1, (1,1))))
+    assert(intLiter.runParser("0b001").contains(IntLiter(1, (1, 1))))
   }
 
   ignore("Successfully parses binary integer with positive sign") {
-    assert(intLiter.runParser("+0b11").contains(IntLiter(3, (1,1))))
+    assert(intLiter.runParser("+0b11").contains(IntLiter(3, (1, 1))))
   }
 
   ignore("Successfully parses binary integer with negative sign") {
-    assert(intLiter.runParser("-0b11").contains(IntLiter(-3, (1,1))))
+    assert(intLiter.runParser("-0b11").contains(IntLiter(-3, (1, 1))))
+  }
+
+  ignore("Successfully fails to parse overflow binary number") {
+    assert(intLiter.runParser("0b10000000000000000000000000000000").isFailure)
+  }
+
+  test("Successfully fails to parse overflow negative binary number")(pending)
+
+  ignore("Successfully fails to parse overflow octal number") {
+    assert(intLiter.runParser("0o20000000000").isFailure)
+  }
+
+  ignore("Successfully fails to parse overflow negative octal number") {
+    assert(intLiter.runParser("-0o20000000000").isFailure)
   }
 
 }
