8e0ee62b9040c802bb9fa3d10d93f3977d0e70dd
S, Devam <devam.savjani19@imperial.ac.uk>
2021-03-11 11:24:04 +0000

    feat(PeepholeStrong): Add strength reduc for div

--- src/main/scala/backend/Peephole/PeepholeStrong.scala
+++ src/main/scala/backend/Peephole/PeepholeStrong.scala
@@ -11,7 +11,11 @@ import scala.math._
 object PeepholeStrong {
 
   def log2(i: Int): Double = {
-    log(i) / log(2.0)
+    if (i == 0) {
+      -0.1
+    } else {
+      log(i) / log(2.0)
+    }
   }
 
   def peepholeStrong(
@@ -69,7 +73,33 @@ object PeepholeStrong {
           }
         case _ =>
       }
-    }
+    } else if (remainingTail.size > 4)
+      if (
+        remainingTail.tail.tail.tail.head == BranchLink(Label("__aeabi_idiv"))
+      ) {
+        op2 match {
+          case ImmMem(n) =>
+            val shiftAmount = log2(n)
+            if (shiftAmount == floor(shiftAmount) && shiftAmount != 0) {
+              remainingTail.head match {
+                case Mov(rd, _) =>
+                  instructionsBuff += Ldr(r1, op1)
+                  instructionsBuff += Mov(
+                    r1,
+                    LSR(r1, ImmInt(shiftAmount.toInt))
+                  )
+                  instructionsBuff += Mov(rd, r1)
+                  instructionsBuff ++= optimise(
+                    remainingTail.tail.tail.tail.tail.head,
+                    remainingTail.tail.tail.tail.tail.tail
+                  )
+                  return instructionsBuff
+                case _ =>
+              }
+            }
+          case _ =>
+        }
+      }
     instructionsBuff += Ldr(r1, op1)
     instructionsBuff ++= optimise(Ldr(r2, op2), remainingTail)
     instructionsBuff
