9fef8ee744e5dccac3e7be5ea5dffe71eaf26007
D, Tyrell <tyrell.duku19@imperial.ac.uk>
2021-03-12 22:40:54 +0000

    test(BitwiseTest): Remove "ignore"
    Co-authored-by: R, Alex <alex.richardson19@imperial.ac.uk>

--- src/test/scala/backend/BackendBitwiseTest.scala
+++ src/test/scala/backend/BackendBitwiseTest.scala
@@ -5,10 +5,10 @@ class BackendBitwiseTest extends AnyFunSuite {
   for (f <- getFilesFrom("wacc_examples/valid/bitwise/")) {
     val name = f.getName
     val (file, out, command) = createOutputFiles(f)
-    ignore(s"Bitwise exit code test for $name") {
+    test(s"Bitwise exit code test for $name") {
       assert(checkExitCode(file, command))
     }
-    ignore(s"Bitwise expected test for $name") {
+    test(s"Bitwise expected test for $name") {
       assert(checkStdOut(file, out))
     }
   }
