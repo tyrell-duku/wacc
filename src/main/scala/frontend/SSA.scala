5473e22d00083ab248f929dd15c4107609db113f
R, Alex <alex.richardson19@imperial.ac.uk>
2021-03-13 17:23:07 +0000

    feat(SSA): Add transformStat cases
    - Finish transformExpr
    - Add updateRhs, updateLhs
    - Rename foo to deadCodeElimination
    
    Co-authored-by: P, Jaimi <jaimi.patel19@imperial.ac.uk>

--- src/main/scala/frontend/SSA.scala
+++ src/main/scala/frontend/SSA.scala
@@ -37,10 +37,44 @@ object SSA {
   }
 
   private def transformExpr(e: Expr): Expr = e match {
-    case Plus(l, r, pos) => Plus(transformExpr(l), transformExpr(r), pos)
+    case DerefPtr(ptr, pos) => DerefPtr(transformExpr(ptr), pos)
+    case Addr(ptr, pos)     => Addr(transformExpr(ptr), pos)
+    case sizeof: SizeOf     => sizeof
+    case Not(e, pos)        => Not(transformExpr(e), pos)
+    case Negation(e, pos)   => Negation(transformExpr(e), pos)
+    case Len(e, pos)        => Len(transformExpr(e), pos)
+    case Ord(e, pos)        => Ord(transformExpr(e), pos)
+    case Chr(e, pos)        => Chr(transformExpr(e), pos)
+    case BitwiseNot(e, pos) => BitwiseNot(transformExpr(e), pos)
+    case Mul(l, r, pos)     => Mul(transformExpr(l), transformExpr(r), pos)
+    case Div(l, r, pos)     => Div(transformExpr(l), transformExpr(r), pos)
+    case Mod(l, r, pos)     => Mod(transformExpr(l), transformExpr(r), pos)
+    case Plus(l, r, pos)    => Plus(transformExpr(l), transformExpr(r), pos)
+    case Sub(l, r, pos)     => Sub(transformExpr(l), transformExpr(r), pos)
+    case GT(l, r, pos)      => GT(transformExpr(l), transformExpr(r), pos)
+    case GTE(l, r, pos)     => GTE(transformExpr(l), transformExpr(r), pos)
+    case LT(l, r, pos)      => LT(transformExpr(l), transformExpr(r), pos)
+    case LTE(l, r, pos)     => LTE(transformExpr(l), transformExpr(r), pos)
+    case Equal(l, r, pos)   => Equal(transformExpr(l), transformExpr(r), pos)
+    case NotEqual(l, r, pos) =>
+      NotEqual(transformExpr(l), transformExpr(r), pos)
+    case And(l, r, pos) => And(transformExpr(l), transformExpr(r), pos)
+    case Or(l, r, pos)  => Or(transformExpr(l), transformExpr(r), pos)
+    case BitwiseAnd(l, r, pos) =>
+      BitwiseAnd(transformExpr(l), transformExpr(r), pos)
+    case BitwiseOr(l, r, pos) =>
+      BitwiseOr(transformExpr(l), transformExpr(r), pos)
+    case BitwiseXor(l, r, pos) =>
+      BitwiseXor(transformExpr(l), transformExpr(r), pos)
+    case LogicalShiftLeft(l, r, pos) =>
+      Plus(transformExpr(l), transformExpr(r), pos)
+    case LogicalShiftRight(l, r, pos) =>
+      Plus(transformExpr(l), transformExpr(r), pos)
     case id: Ident =>
-      val Ident(x, pos) = updateIdent(id)
-      kvs(x)
+      val id2 @ Ident(x, _) = updateIdent(id)
+      kvs.getOrElse(x, id2)
+    case ArrayElem(id, es, pos) =>
+      ArrayElem(updateIdent(id), es.map(transformExpr), pos)
     case x: IntLiter  => x
     case b: BoolLiter => b
     case c: CharLiter => c
@@ -48,9 +82,27 @@ object SSA {
     case p: PairLiter => p
   }
 
-  private def foo(rhs: AssignRHS, stat: Stat): Stat = rhs match {
-    case _: Expr => null
-    case _       => stat
+  private def deadCodeElimination(rhs: AssignRHS, stat: Stat): Stat =
+    rhs match {
+      case _: Expr => null
+      case _       => stat
+    }
+
+  private def updateRhs(rhs: AssignRHS): AssignRHS = rhs match {
+    case e: Expr => transformExpr(e)
+    case _       => rhs
+  }
+
+  private def updateLhs(lhs: AssignLHS): AssignLHS = lhs match {
+    case DerefPtr(ptr, pos)  => DerefPtr(transformExpr(ptr), pos)
+    case Fst(id: Ident, pos) => Fst(updateIdent(id), pos)
+    case Snd(id: Ident, pos) => Snd(updateIdent(id), pos)
+    // Not for EqAssign
+    case id: Ident => updateIdent(id)
+    case ArrayElem(id, es, pos) =>
+      ArrayElem(updateIdent(id), es.map(transformExpr), pos)
+    // Semantically incorrect
+    case _ => ???
   }
 
   /* Transforms a given statement S into SSA form. */
@@ -58,14 +110,21 @@ object SSA {
     s match {
       case EqIdent(t, id @ Ident(_, pos), r) =>
         val v = addToHashMap(id, r)
-        foo(r, EqIdent(t, v, r))
+        deadCodeElimination(r, EqIdent(t, v, r))
       case EqAssign(id: Ident, r) =>
         val v = addToHashMap(id, r)
-        foo(r, EqAssign(v, r))
-      case PrintLn(e) => PrintLn(transformExpr(e))
+        deadCodeElimination(r, EqAssign(v, r))
+      case EqAssign(lhs, rhs) => EqAssign(updateLhs(lhs), updateRhs(rhs))
+      case Read(lhs)          => Read(updateLhs(lhs))
+      case Free(e)            => Free(transformExpr(e))
+      case Return(e)          => Return(transformExpr(e))
+      case Exit(e)            => Exit(transformExpr(e))
+      case Print(e)           => Print(transformExpr(e))
+      case PrintLn(e)         => PrintLn(transformExpr(e))
       case Seq(statList) =>
         Seq(statList.map(transformStat).filter(x => x != null))
-      case _ => ???
+      case Skip => Skip
+      case _    => ???
     }
   }
 
