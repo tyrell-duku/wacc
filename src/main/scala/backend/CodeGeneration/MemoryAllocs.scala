077fff16cafdbf862ea8c6ec5f38551345ba17aa
P, Jaimi <jaimi.patel19@imperial.ac.uk>
2021-03-13 03:52:58 +0000

    feat(MemoryAllocs): Add pointerArith func
    - Called in add/sub, used for pointer arithmetic

--- src/main/scala/backend/CodeGeneration/MemoryAllocs.scala
+++ src/main/scala/backend/CodeGeneration/MemoryAllocs.scala
@@ -3,8 +3,11 @@ package backend.CodeGeneration
 import scala.collection.mutable.ListBuffer
 import backend.CodeGeneration.Expressions.transExp
 import backend.CodeGenerator._
+import backend.DefinedFuncs.PreDefinedFuncs.Overflow
+import backend.DefinedFuncs.RuntimeErrors.addRuntimeError
 import backend.IR.InstructionSet._
 import backend.IR.Operand._
+import backend.IR.Condition.{NE, VS}
 import frontend.Rules._
 
 object MemoryAllocs {
@@ -30,6 +33,27 @@ object MemoryAllocs {
         addUnusedReg(nextFreeReg)
       case Realloc(ptr, size, _) =>
       case Calloc(num, size, _)  =>
+    }
+    instructions
+  }
+
+  def pointerArith(l: Expr, rd: Reg): ListBuffer[Instruction] = {
+    val instructions = ListBuffer.empty[Instruction]
+    val t = getExprType(l)
+
+    if (t.isPtr) {
+      val PtrT(inner) = t
+      val varSize = getBaseTypeSize(inner)
+      if (varSize > CHAR_SIZE) {
+        val rm = getFreeReg()
+        instructions += Ldr(rm, ImmMem(varSize))
+        // Runtime error check
+        instructions += SMul(rd, rm, rd, rm)
+        instructions += Cmp(rm, ASR(rd, ImmInt(31)))
+        addUnusedReg(rm)
+        instructions += BranchLinkCond(NE, addRuntimeError(Overflow))
+      }
+
     }
     instructions
   }
