22e40335499bfd7f8ed6d8ef93db22ab1442940c
R, Alex <alex.richardson19@imperial.ac.uk>
2021-03-14 20:22:52 +0000

    refactor(SSA): Use .map
    - Replace repeated pattern matching with .map case

--- src/main/scala/frontend/SSA.scala
+++ src/main/scala/frontend/SSA.scala
@@ -49,39 +49,7 @@ object SSA {
   }
 
   private def transformExpr(e: Expr): Expr = e match {
-    case DerefPtr(ptr, pos) => DerefPtr(transformExpr(ptr), pos)
-    case Addr(ptr, pos)     => Addr(transformExpr(ptr), pos)
-    case sizeof: SizeOf     => sizeof
-    case Not(e, pos)        => Not(transformExpr(e), pos)
-    case Negation(e, pos)   => Negation(transformExpr(e), pos)
-    case Len(e, pos)        => Len(transformExpr(e), pos)
-    case Ord(e, pos)        => Ord(transformExpr(e), pos)
-    case Chr(e, pos)        => Chr(transformExpr(e), pos)
-    case BitwiseNot(e, pos) => BitwiseNot(transformExpr(e), pos)
-    case Mul(l, r, pos)     => Mul(transformExpr(l), transformExpr(r), pos)
-    case Div(l, r, pos)     => Div(transformExpr(l), transformExpr(r), pos)
-    case Mod(l, r, pos)     => Mod(transformExpr(l), transformExpr(r), pos)
-    case Plus(l, r, pos)    => Plus(transformExpr(l), transformExpr(r), pos)
-    case Sub(l, r, pos)     => Sub(transformExpr(l), transformExpr(r), pos)
-    case GT(l, r, pos)      => GT(transformExpr(l), transformExpr(r), pos)
-    case GTE(l, r, pos)     => GTE(transformExpr(l), transformExpr(r), pos)
-    case LT(l, r, pos)      => LT(transformExpr(l), transformExpr(r), pos)
-    case LTE(l, r, pos)     => LTE(transformExpr(l), transformExpr(r), pos)
-    case Equal(l, r, pos)   => Equal(transformExpr(l), transformExpr(r), pos)
-    case NotEqual(l, r, pos) =>
-      NotEqual(transformExpr(l), transformExpr(r), pos)
-    case And(l, r, pos) => And(transformExpr(l), transformExpr(r), pos)
-    case Or(l, r, pos)  => Or(transformExpr(l), transformExpr(r), pos)
-    case BitwiseAnd(l, r, pos) =>
-      BitwiseAnd(transformExpr(l), transformExpr(r), pos)
-    case BitwiseOr(l, r, pos) =>
-      BitwiseOr(transformExpr(l), transformExpr(r), pos)
-    case BitwiseXor(l, r, pos) =>
-      BitwiseXor(transformExpr(l), transformExpr(r), pos)
-    case LogicalShiftLeft(l, r, pos) =>
-      Plus(transformExpr(l), transformExpr(r), pos)
-    case LogicalShiftRight(l, r, pos) =>
-      Plus(transformExpr(l), transformExpr(r), pos)
+    case sizeof: SizeOf => sizeof
     case id: Ident =>
       val id2 @ Ident(x, _) = updateIdent(id)
       kvs.getOrElse(x, id2)
@@ -89,13 +57,13 @@ object SSA {
       val arrayElemName = arrayElemIdentifier(s, es)
       val (_, y) = dict(arrayElemName)
       kvs(y.toString + arrayElemName)
-    // TODO: make Liter class in Rules for pattern match?
-    // case class Liters extends Expr
     case x: IntLiter  => x
     case b: BoolLiter => b
     case c: CharLiter => c
     case s: StrLiter  => s
     case p: PairLiter => p
+    // All operators
+    case _ => e.map(transformExpr)
   }
 
   private def deadCodeElimination(
@@ -156,7 +124,7 @@ object SSA {
       case Print(e)   => buf += Print(transformExpr(e))
       case PrintLn(e) => buf += PrintLn(transformExpr(e))
       case If(cond, s1, s2) =>
-        val BoolLiter(b, _) = foldExpr(cond)
+        val BoolLiter(b, _) = foldExpr(transformExpr(cond))
         if (b) buf ++= transformStat(s1) else buf ++= transformStat(s2)
       case Seq(statList) => statList.flatMap(transformStat).to(ListBuffer)
       case Skip          => buf += Skip
@@ -174,9 +142,7 @@ object SSA {
   /* Transforms a given program AST into SSA form. */
   def toSSA(ast: Program): Program = {
     val Program(fs, s) = ast
-    val x = Program(fs.map(f => transformFunc(f)), Seq(transformStat(s).toList))
-    println(x)
-    x
+    Program(fs.map(f => transformFunc(f)), Seq(transformStat(s).toList))
   }
 
 }
