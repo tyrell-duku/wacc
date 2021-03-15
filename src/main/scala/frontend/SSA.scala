25b00cb47291ed0a3f0282ea846cb7ae4981b5ac
P, Jaimi <jaimi.patel19@imperial.ac.uk>
2021-03-15 18:41:20 +0000

    feat(SSA): Add dead code elim for Arrays
    - ArrayLiterals are only added to code if necessary
    - ArrayElems assignments are no longer present unless there is a read statement
    - Change kvs to store assignRhs
    Co-authored-by: S, Devam <devam.savjani19@imperial.ac.uk>
    
    Co-authored-by: R, Alex <alex.richardson19@imperial.ac.uk>
    
    Co-authored-by: D, Tyrell <tyrell.duku19@imperial.ac.uk>

--- src/main/scala/frontend/SSA.scala
+++ src/main/scala/frontend/SSA.scala
@@ -4,12 +4,12 @@ import Rules._
 import scala.collection.immutable.HashMap
 import scala.collection.mutable.ListBuffer
 import backend.CodeGenerator.getExprType
-import ConstantFolding.foldExpr
+import ConstantFolding.{foldExpr, foldIntOps}
 
 object SSA {
   // <Identifier's name, (LHS unique identifier, RHS unique identifier)>
   private var dict = new HashMap[String, (Int, Int)]
-  private var kvs = new HashMap[String, Expr]
+  private var kvs = new HashMap[String, AssignRHS]
   val INITIAL_LHS = 0
   val INITIAL_RHS = 0
 
@@ -26,6 +26,7 @@ object SSA {
     rhs match {
       case e: Expr => kvs += ((v, transformExpr(e)))
       case arr: ArrayLiter =>
+        kvs += ((v, arr))
         arr match {
           case ArrayLiter(Some(es), _) =>
             for (i <- es.indices) {
@@ -53,13 +54,15 @@ object SSA {
     case sizeof: SizeOf => sizeof
     case id: Ident =>
       val id2 @ Ident(x, _) = updateIdent(id)
-      kvs.getOrElse(x, id2)
+      toExpr(kvs.getOrElse(x, id2))
     case ArrayElem(id @ Ident(s, _), es, pos) =>
       val arrayElemName = arrayElemIdentifier(s, es)
       val (_, y) = dict(arrayElemName)
-      kvs.getOrElse(
-        y.toString + arrayElemName,
-        Ident(y.toString + arrayElemName, null)
+      toExpr(
+        kvs.getOrElse(
+          y.toString + arrayElemName,
+          Ident(y.toString + arrayElemName, null)
+        )
       )
     case x: IntLiter  => x
     case b: BoolLiter => b
@@ -76,13 +79,19 @@ object SSA {
       buf: ListBuffer[Stat]
   ): ListBuffer[Stat] =
     rhs match {
-      case _: Expr => buf
-      case _       => buf += stat
+      case _: Expr | _: ArrayLiter => buf
+      case _                       => buf += stat
     }
 
   private def updateRhs(rhs: AssignRHS): AssignRHS = rhs match {
     case e: Expr => transformExpr(e)
     case _       => rhs
+  }
+
+  private def toExpr(rhs: AssignRHS): Expr = rhs match {
+    case e: Expr => e
+    // invalid
+    case _ => ???
   }
 
   private def arrayElemIdentifier(varName: String, es: List[Expr]): String = {
@@ -99,6 +108,11 @@ object SSA {
     case _ => ???
   }
 
+  private def intLiterToInt(e: Expr): Int = e match {
+    case IntLiter(n, _) => n
+    case _              => ???
+  }
+
   /* Transforms a given statement S into SSA form. */
   private def transformStat(s: Stat): ListBuffer[Stat] = {
     val buf = ListBuffer.empty[Stat]
@@ -109,13 +123,22 @@ object SSA {
       case EqAssign(id: Ident, r) =>
         val v = addToHashMap(id, r)
         deadCodeElimination(r, EqAssign(v, r), buf)
-      case EqAssign(ArrayElem(id @ Ident(s, idPos), es, pos), r) =>
-        addToHashMap(Ident(arrayElemIdentifier(s, es), idPos), r)
+      case EqAssign(ArrayElem(id, es, pos), e: Expr) =>
+        val Ident(s, _) = updateIdent(id)
+        val newArray = kvs(s) match {
+          case ArrayLiter(Some(elems), pos) =>
+            val index =
+              es.map(x => foldIntOps(transformExpr(x))).map(intLiterToInt)
+            ArrayLiter(Some(elems.updated(index(0), transformExpr(e))), pos)
+          // TODO: out of bound
+          case _ => ???
+        }
+        addToHashMap(id, newArray)
         buf
       case EqAssign(lhs, rhs) => buf += EqAssign(updateLhs(lhs), updateRhs(rhs))
       case Read(id: Ident) =>
         val Ident(s, _) = updateIdent(id)
-        val e = kvs(s)
+        val e = toExpr(kvs(s))
         // add to dict but not kvs
         val v = addToHashMap(id, null)
         buf += EqIdent(getExprType(e), v, e)
@@ -123,23 +146,43 @@ object SSA {
       case Read(ArrayElem(id @ Ident(s, _), es, pos)) =>
         val arrayElemName = arrayElemIdentifier(s, es)
         val (_, y) = dict(arrayElemName)
-        val e = kvs(y.toString + arrayElemName)
+        val e = toExpr(kvs(y.toString + arrayElemName))
         val v = addToHashMap(Ident(arrayElemName, null), null)
         buf += EqIdent(getExprType(e), v, e)
         buf += Read(v)
       case Read(lhs)  => buf += Read(updateLhs(lhs))
-      case Free(e)    => buf += Free(transformExpr(e))
-      case Return(e)  => buf += Return(transformExpr(e))
+      case Free(e)    => buf ++= transExpArray(e, Free)
+      case Return(e)  => buf ++= transExpArray(e, Return)
       case Exit(e)    => buf += Exit(transformExpr(e))
-      case Print(e)   => buf += Print(transformExpr(e))
-      case PrintLn(e) => buf += PrintLn(transformExpr(e))
-      case If(cond, s1, s2) =>
-        val BoolLiter(b, _) = foldExpr(transformExpr(cond))
-        if (b) buf ++= transformStat(s1) else buf ++= transformStat(s2)
+      case Print(e)   => buf ++= transExpArray(e, Print)
+      case PrintLn(e) => buf ++= transExpArray(e, PrintLn)
+      // case If(cond, s1, s2) =>
+      //   val BoolLiter(b, _) = foldExpr(transformExpr(cond))
+      //   if (b) buf ++= transformStat(s1) else buf ++= transformStat(s2)
       case Seq(statList) => statList.flatMap(transformStat).to(ListBuffer)
       case Skip          => buf += Skip
-      case _             => buf
+      case s             => buf += s
     }
+  }
+
+  private def transExpArray(e: Expr, pf: (Expr => Stat)): ListBuffer[Stat] = {
+    val buf = ListBuffer.empty[Stat]
+    e match {
+      case id: Ident =>
+        val id2 @ Ident(x, _) = updateIdent(id)
+        val rhs = kvs.getOrElse(x, id2)
+        rhs match {
+          // Not array, transformExpr as usual
+          case _: Expr => buf += pf(toExpr(rhs))
+          // Array has not been defined then add to list of Statements
+          case _ =>
+            buf += EqIdent(rhs.getType(null), id2, rhs)
+            buf += pf(id2)
+        }
+      // Not array, transformExpr as usual
+      case _ => buf += pf(transformExpr(e))
+    }
+    buf
   }
 
   /* Transforms a given function F into SSA form. */
