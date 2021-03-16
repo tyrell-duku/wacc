8483b3e84fbaf7a839c681a5ad7f2bf70ab34619
R, Alex <alex.richardson19@imperial.ac.uk>
2021-03-16 16:58:08 +0000

    fix(ConstantFolding): Add runtime error checks
    - Add checkOverflow, evalConditionally
    Co-authored-by: D, Tyrell <tyrell.duku19@imperial.ac.uk>

--- src/main/scala/frontend/ConstantFolding.scala
+++ src/main/scala/frontend/ConstantFolding.scala
@@ -1,167 +1,72 @@
 package frontend
 
 import Rules._
+import frontend.LiterParser.notOverflow
 import backend.CodeGenerator.getBaseTypeSize
 
 object ConstantFolding {
 
-  /* Folds an application of OP on two integer operands, returning the result. */
+  /* Checks whether either argument X or Y over/underflow or the application of
+     a operation OP with X and Y as its arguments over/underflows. */
+  def checkOverflow(x: Int, y: Int, op: (Long, Long) => Long): Boolean = {
+    var overflown = false
+    val xLong = x.toLong
+    val yLong = y.toLong
+    if (xLong > Integer.MAX_VALUE || xLong < Integer.MIN_VALUE) {
+      overflown = true
+    }
+    if (yLong > Integer.MAX_VALUE || yLong < Integer.MIN_VALUE) {
+      overflown = true
+    }
+    val result = op(xLong, yLong)
+    if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+      overflown = true
+    }
+    overflown
+  }
+
+  private def evalConditionally(
+      n1: Int,
+      n2: Int,
+      op: (Long, Long) => Long,
+      pos: (Int, Int)
+  ): Expr = {
+    if (checkOverflow(n1, n2, op)) null else IntLiter(op(n1, n2).toInt, pos)
+  }
+
+  /* Folds an application of OP on two integer operands, returning the result.
+     The result will be null if a runtime error has occurred. */
   def foldIntOps(op: Expr): Expr = op match {
     // Base case
     case n: IntLiter => n
-    // Folding
-    case Mul(IntLiter(n1, p), IntLiter(n2, _), _)        => IntLiter(n1 * n2, p)
-    case Div(IntLiter(n1, p), IntLiter(n2, _), _)        => IntLiter(n1 / n2, p)
-    case Plus(IntLiter(n1, p), IntLiter(n2, _), _)       => IntLiter(n1 + n2, p)
-    case Sub(IntLiter(n1, p), IntLiter(n2, _), _)        => IntLiter(n1 - n2, p)
-    case Mod(IntLiter(n1, p), IntLiter(n2, _), _)        => IntLiter(n1 % n2, p)
-    case BitwiseAnd(IntLiter(n1, p), IntLiter(n2, _), _) => IntLiter(n1 & n2, p)
-    case BitwiseOr(IntLiter(n1, p), IntLiter(n2, _), _)  => IntLiter(n1 | n2, p)
-    case BitwiseXor(IntLiter(n1, p), IntLiter(n2, _), _) => IntLiter(n1 ^ n2, p)
+    // UnOp folding
+    // TODO: overflow
+    case Negation(IntLiter(n, _), pos)   => IntLiter(-n, pos)
+    case BitwiseNot(IntLiter(n, _), pos) => IntLiter(~n, pos)
+    case SizeOf(t, pos)                  => IntLiter(getBaseTypeSize(t), pos)
+    // BinOp folding
+    case Mul(IntLiter(n1, p), IntLiter(n2, _), _) =>
+      evalConditionally(n1, n2, (_ * _), p)
+    case Div(IntLiter(n1, p), IntLiter(n2, _), _) =>
+      if (n2 == 0) null else evalConditionally(n1, n2, (_ / _), p)
+    case Plus(IntLiter(n1, p), IntLiter(n2, _), _) =>
+      evalConditionally(n1, n2, (_ + _), p)
+    case Sub(IntLiter(n1, p), IntLiter(n2, _), _) =>
+      evalConditionally(n1, n2, (_ - _), p)
+    case Mod(IntLiter(n1, p), IntLiter(n2, _), _) =>
+      evalConditionally(n1, n2, (_ % _), p)
+    case BitwiseAnd(IntLiter(n1, p), IntLiter(n2, _), _) =>
+      evalConditionally(n1, n2, (_ & _), p)
+    case BitwiseOr(IntLiter(n1, p), IntLiter(n2, _), _) =>
+      evalConditionally(n1, n2, (_ | _), p)
+    case BitwiseXor(IntLiter(n1, p), IntLiter(n2, _), _) =>
+      evalConditionally(n1, n2, (_ ^ _), p)
     case LogicalShiftLeft(IntLiter(n1, p), IntLiter(n2, _), _) =>
-      IntLiter(n1 << n2, p)
+      if (n1 < 0 || n2 < 0) null else evalConditionally(n1, n2, (_ << _), p)
     case LogicalShiftRight(IntLiter(n1, p), IntLiter(n2, _), _) =>
-      IntLiter(n1 >> n2, p)
+      if (n1 < 0 || n2 < 0) null else evalConditionally(n1, n2, (_ >> _), p)
     // Recursive case (ArithOps & BitwiseOps)
-    case _ => foldIntOps(op.map(foldIntOps))
+    case _: ArithOps | _: BitwiseOps | _: UnOp => foldIntOps(op.map(foldIntOps))
+    case e                                     => e
   }
-
-  /* Folds an expression E, returning its result. */
-  def foldExpr(e: Expr): Expr = e match {
-    // case DerefPtr(ptr, pos)                   =>
-    // case Addr(ptr, pos)                       =>
-    // case Len(e, pos)                          =>
-    // case BitwiseNot(e, pos)                   =>
-    // case Equal(lExpr, rExpr, pos)             =>
-    // case NotEqual(lExpr, rExpr, pos)          =>
-    // case BitwiseAnd(lExpr, rExpr, pos)        =>
-    // case BitwiseOr(lExpr, rExpr, pos)         =>
-    // case BitwiseXor(lExpr, rExpr, pos)        =>
-    // case LogicalShiftLeft(lExpr, rExpr, pos)  =>
-    // case LogicalShiftRight(lExpr, rExpr, pos) =>
-    case op: SizeOf =>
-      val (num, b) = foldInt(op)
-      if (b) IntLiter(num, null) else op
-    case op: Ord =>
-      val (num, b) = foldInt(op)
-      if (b) IntLiter(num, null) else op
-    case op: Negation =>
-      val (num, b) = foldInt(op)
-      if (b) IntLiter(num, null) else op
-    case op: ArithOps =>
-      val (num, b) = foldInt(op)
-      if (b) IntLiter(num, null) else op
-    case e: Expr => e
-  }
-
-  /* Folds a Boolean expression E, returning its result. */
-  def foldBool(e: Expr): (Boolean, Boolean) = e match {
-    case BoolLiter(b, _) => (b, true)
-    case And(l, r, _) =>
-      val (n1, b1) = foldBool(l)
-      val (n2, b2) = foldBool(r)
-      (n1 && n2, b1 && b2)
-    case Or(l, r, _) =>
-      val (n1, b1) = foldBool(l)
-      val (n2, b2) = foldBool(r)
-      (n1 || n2, b1 && b2)
-    // case Equal(l, r, _)    => foldBool(l) == foldBool(r)
-    // case NotEqual(l, r, _) => foldBool(l) != foldBool(r)
-    case GT(l, r, _) =>
-      l.getType(null) match {
-        case CharT =>
-          val (c1, b1) = foldChar(l)
-          val (c2, b2) = foldChar(r)
-          (c1 > c2, b1 && b2)
-        case IntT =>
-          val (n1, b1) = foldInt(l)
-          val (n2, b2) = foldInt(r)
-          (n1 > n2, b1 && b2)
-        case _ => ???
-      }
-    case GTE(l, r, _) =>
-      l.getType(null) match {
-        case CharT =>
-          val (c1, b1) = foldChar(l)
-          val (c2, b2) = foldChar(r)
-          (c1 >= c2, b1 && b2)
-        case IntT =>
-          val (n1, b1) = foldInt(l)
-          val (n2, b2) = foldInt(r)
-          (n1 >= n2, b1 && b2)
-        case _ => ???
-      }
-    case LT(l, r, _) =>
-      l.getType(null) match {
-        case CharT =>
-          val (c1, b1) = foldChar(l)
-          val (c2, b2) = foldChar(r)
-          (c1 < c2, b1 && b2)
-        case IntT =>
-          val (n1, b1) = foldInt(l)
-          val (n2, b2) = foldInt(r)
-          (n1 < n2, b1 && b2)
-        case _ => ???
-      }
-    case LTE(l, r, _) =>
-      l.getType(null) match {
-        case CharT =>
-          val (c1, b1) = foldChar(l)
-          val (c2, b2) = foldChar(r)
-          (c1 <= c2, b1 && b2)
-        case IntT =>
-          val (n1, b1) = foldInt(l)
-          val (n2, b2) = foldInt(r)
-          (n1 <= n2, b1 && b2)
-        case _ => ???
-      }
-    case Not(b, _) =>
-      val (n1, b1) = foldBool(b)
-      (!n1, b1)
-    case _ => (false, false)
-  }
-
-  /* Folds an integer expression E, returning the result. */
-  def foldInt(e: Expr): (Int, Boolean) = e match {
-    case IntLiter(n, _) => (n, true)
-    case Plus(l, r, _) =>
-      val (n1, b1) = foldInt(l)
-      val (n2, b2) = foldInt(r)
-      (n1 + n2, b1 && b2)
-    case Sub(l, r, _) =>
-      val (n1, b1) = foldInt(l)
-      val (n2, b2) = foldInt(r)
-      (n1 - n2, b1 && b2)
-    case Div(l, r, _) =>
-      val (n1, b1) = foldInt(l)
-      val (n2, b2) = foldInt(r)
-      (n1 / n2, b1 && b2)
-    case Mul(l, r, _) =>
-      val (n1, b1) = foldInt(l)
-      val (n2, b2) = foldInt(r)
-      (n1 * n2, b1 && b2)
-    case Negation(e, _) =>
-      val (n1, b1) = foldInt(e)
-      (-n1, b1)
-    case Mod(l, r, _) =>
-      val (n1, b1) = foldInt(l)
-      val (n2, b2) = foldInt(r)
-      (n1 % n2, b1 && b2)
-    case SizeOf(t, _) =>
-      (getBaseTypeSize(t), true)
-    case Ord(e, _) =>
-      val (c, b1) = foldChar(e)
-      (c.toInt, b1)
-    case _ => (0, false)
-  }
-
-  /* Folds a character expression E, returning its result. */
-  def foldChar(e: Expr): (Char, Boolean) = e match {
-    case CharLiter(NormalChar(c), _) => (c, true)
-    case Chr(e, _) =>
-      val (n, b1) = foldInt(e)
-      (n.toChar, b1)
-    case _ => ('a', false)
-  }
-
 }
