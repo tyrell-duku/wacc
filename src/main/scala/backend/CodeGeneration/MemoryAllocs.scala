2589d85c44c8e049452d5c3259879285bebf24f2
P, Jaimi <jaimi.patel19@imperial.ac.uk>
2021-03-18 15:15:47 +0000

    style(MemoryAllocs): Add comments

--- src/main/scala/backend/CodeGeneration/MemoryAllocs.scala
+++ src/main/scala/backend/CodeGeneration/MemoryAllocs.scala
@@ -11,43 +11,54 @@ import backend.IR.Condition.{NE, VS}
 import frontend.Rules._
 
 object MemoryAllocs {
+  /* Translates malloc, realloc and calloc memory allocations into our IR
+     instruction set. */
   def transMemoryAlloc(
       t: Type,
       elem: MemoryAlloc,
       freeReg: Reg
   ): ListBuffer[Instruction] = {
     val instructions = ListBuffer.empty[Instruction]
+    // For a memory alloc to occur type must be a pointer type
     val PtrT(inner) = t
-    val varSize = getBaseTypeSize(inner)
     elem match {
       case Malloc(size, _) =>
         instructions ++= transExp(size, freeReg)
         instructions += Mov(resultReg, freeReg)
+        // Move size into R0 for branch to malloc
         instructions += BranchLink(Label("malloc"))
       case Realloc(ptr, size, _) =>
         instructions ++= transExp(size, freeReg)
         instructions += Mov(R1, freeReg)
         instructions ++= transExp(ptr, freeReg)
         instructions += Mov(R0, freeReg)
+        // Move ptr address into R0 and new size to R1 for branch to realloc
         instructions += BranchLink(Label("realloc"))
       case Calloc(num, size, _) =>
         instructions ++= transExp(size, freeReg)
         instructions += Mov(R1, freeReg)
         instructions ++= transExp(num, freeReg)
         instructions += Mov(R0, freeReg)
+        // Move number into R0 and size per var to R1 for branch to calloc
         instructions += BranchLink(Label("calloc"))
     }
     instructions += Mov(freeReg, resultReg)
     instructions
   }
 
+  /* Called upon for any pointer arithmetic in Add or Sub cases. Increments
+     the pointer by the correct amount (1 or 4 bytes) by checking its type
+     and subsequent base type size.  */
   def pointerArith(l: Expr, rd: Reg): ListBuffer[Instruction] = {
     val instructions = ListBuffer.empty[Instruction]
     val t = getExprType(l)
-
+    // Pointer arithmetic should only occur in add/subtract if the lhs is a 
+    // pointer.
     if (t.isPtr) {
       val PtrT(inner) = t
       val varSize = getBaseTypeSize(inner)
+      // If varSize is greater than 1 byte (typically 4 bytes) then add or
+      // subtract by relevant variable size.
       if (varSize > CHAR_SIZE) {
         val rm = getFreeReg()
         instructions += Ldr(rm, ImmMem(varSize))
