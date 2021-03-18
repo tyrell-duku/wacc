25cfba6dbb9d399ce562a5348d23061795fbec08
P, Jaimi <jaimi.patel19@imperial.ac.uk>
2021-03-18 02:51:48 +0000

    test(BackendIOTest): Remove skip
    Co-authored-by: S, Devam <devam.savjani19@imperial.ac.uk>

--- src/test/scala/backend/BackendIOTest.scala
+++ src/test/scala/backend/BackendIOTest.scala
@@ -5,10 +5,10 @@ class BackendIOTest extends AnyFunSuite {
   for (f <- getFilesFrom("wacc_examples/valid/IO/")) {
     val name = f.getName()
     val (file, out, command) = createOutputFiles(f)
-    ignore(s"IO exit code test for $name") {
+    test(s"IO exit code test for $name") {
       assert(checkExitCode(file, command))
     }
-    ignore(s"IO expected test for $name") {
+    test(s"IO expected test for $name") {
       assert(checkStdOut(file, out))
     }
   }
