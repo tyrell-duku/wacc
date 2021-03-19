7d0ca2d984e39ac3d106e51f64e226799786d4a3
S, Devam <devam.savjani19@imperial.ac.uk>
2021-03-19 01:27:54 +0000

    test(BackendArrayTest): Change ignore to test
    Co-authored-by: P, Jaimi <jaimi.patel19@imperial.ac.uk>
    
    Co-authored-by: R, Alex <alex.richardson19@imperial.ac.uk>

--- src/test/scala/backend/BackendArrayTest.scala
+++ src/test/scala/backend/BackendArrayTest.scala
@@ -5,10 +5,10 @@ class BackendArrayTest extends AnyFunSuite {
   for (f <- getFilesFrom("wacc_examples/valid/array/")) {
     val name = f.getName
     val (file, out, command) = createOutputFiles(f)
-    ignore(s"Array exit code test for $name") {
+    test(s"Array exit code test for $name") {
       assert(checkExitCode(file, command))
     }
-    ignore(s"Array expected test for $name") {
+    test(s"Array expected test for $name") {
       assert(checkStdOut(file, out))
     }
   }
