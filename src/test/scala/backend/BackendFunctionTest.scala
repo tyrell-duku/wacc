29865c1e8f9aff68624abcb8fb2235acfcf20e9c
S, Devam <devam.savjani19@imperial.ac.uk>
2021-03-19 18:30:25 +0000

    test(BackendFunctionTest): Remove ignore

--- src/test/scala/backend/BackendFunctionTest.scala
+++ src/test/scala/backend/BackendFunctionTest.scala
@@ -5,10 +5,10 @@ class BackendFunctionTest extends AnyFunSuite {
   for (f <- getFilesFrom("wacc_examples/valid/function/")) {
     val name = f.getName
     val (file, out, command) = createOutputFiles(f)
-    ignore(s"Function exit code test for $name") {
+    test(s"Function exit code test for $name") {
       assert(checkExitCode(file, command))
     }
-    ignore(s"Function expected test for $name") {
+    test(s"Function expected test for $name") {
       assert(checkStdOut(file, out))
     }
   }
